812ddafce5581d653fb0ee44a13d85eac6222165
Sam Hendrickx <samhendrickx93@gmail.com>
2019-12-01 19:12:04 +0100

    Added all functions.

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -24,37 +24,113 @@ namespace comp110_worksheet_7
 		// Return the total size, in bytes, of all the files below the given directory
 		public static long GetTotalSize(string directory)
 		{
-			throw new NotImplementedException();
+            //declare a list for the files and a variable to store the size of the files
+			String[] files;
+            long size = 0;
+
+            //populate the list with the files and do a foreach loop to store the size of every file in the list.
+            files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+            foreach(string file in files)
+                {
+                    size += GetFileSize(file);
+                }
+            return size;
 		}
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
+            //list to store the files we find in the directory
+			string[] files;
+
+            //Populate the list with the files we find in the given directory
+            files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            //This will return the number of files stored in the list
+            return files.Length;
 		}
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
-			throw new NotImplementedException();
+			string[] depth;
+            int depthValue = 0;
+
+            depth = Directory.GetDirectories(directory);
+            foreach (string dir in depth)
+                {
+                    depthValue++;
+                }
+
+            return depthValue;
 		}
 
 		// Get the path and size (in bytes) of the smallest file below the given directory
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
-			throw new NotImplementedException();
+            //decalre a list variable for all the files and a tuple where we will store the smallest filename and the size
+			string[] files;
+            Tuple<string, long> size;
+
+            //populate the variables
+            //the tuple with dummy information we will overwrite and the list with all the files we find in the given directory
+            size = new Tuple<string, long>("file", long.MaxValue);
+            files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            //select the smallest files from the list using Linq and store in in a variable smallessize
+            //From the filename we got in the smallestFile string we can get the size using the GetFileSize function and store this information into a variable minimumSize
+            string smallestFile = (from file in files let length = GetFileSize(file) where length > 0 orderby length ascending select file).First();
+            long minimumSize = GetFileSize(smallestFile);
+            
+            //store the path of the file into a variable so we can use this to populate the tuple
+            string minimumSizePath = (smallestFile);
+
+            //populate the tuple with the path and size of the smallest file we have found.
+            size = new Tuple<string, long>(minimumSizePath, minimumSize);
+            return size;
 		}
 
 		// Get the path and size (in bytes) of the largest file below the given directory
 		public static Tuple<string, long> GetLargestFile(string directory)
 		{
-			throw new NotImplementedException();
+			string[] files;
+            Tuple<string, long> size;
+
+            size = new Tuple<string, long>("file", long.MaxValue);
+            files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            string biggestFile = (from file in files let length = GetFileSize(file) where length > 0 orderby length descending select file).First();
+            long maximumSize = GetFileSize(biggestFile);
+
+            string maximuSizePath = (biggestFile);
+
+            size = new Tuple<string, long>(maximuSizePath,maximumSize);
+            return size;
+
 		}
 
 		// Get all files whose size is equal to the given value (in bytes) below the given directory
 		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
-			throw new NotImplementedException();
+			string[] files;
+            List<string>filesOfSize = new List<string>();
+            files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+            long fileSize;
+
+            foreach(string file in files)
+                {
+                    fileSize = GetFileSize(file);
+                    if (fileSize == size)
+                    {
+                        filesOfSize.Add(file);
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            return filesOfSize;
+
 		}
 	}
 }
